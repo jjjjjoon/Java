c1540a79a2149779ea983139f173fb6935d15a7b
문재준 <munjaejoon@munjaejun-ui-MacBookPro.local>
2024-10-14 17:35:12 +0900

    자바의 정석 실습

---
+++ practice/MyTest.java
@@ -0,0 +1,26 @@
+//package practice;
+//
+//public class MyTest {
+//    int a;
+//    static int b;
+//
+//    public MyTest(int a) {
+//        this.a = a;
+//    }
+//
+//    public int getA() {
+//        return a;
+//    }
+//
+//    public static int getB() {
+//        return b;
+//    }
+//
+//    public void setA(int a) {
+//        this.a = a;
+//    }
+//
+//    public static void setB(int b) {
+//        MyTest.b = b;
+//    }
+//}
